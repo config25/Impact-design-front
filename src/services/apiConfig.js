ed73568717a33643ccb0af08d069ba330c8e11a3
config25 <yhkim052556@naver.com>
2026-02-25 16:39:04 +0900

    서버 주소 변경

--- src/services/apiConfig.js
+++ src/services/apiConfig.js
@@ -1,4 +1,4 @@
-export const API_BASE = process.env.REACT_APP_API_URL || "http://localhost:8080/api";
+export const API_BASE = process.env.REACT_APP_API_URL || "/api";
 
 let globalLogout = null;
 let isLoggingOut = false;
